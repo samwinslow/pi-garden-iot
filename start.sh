074fa9d8ea1cccadbaac5a399c5de712b18d94c6
Sam Winslow <sammywinslow@gmail.com>
2021-03-30 16:43:28 -0400

    add start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+python3 main.py \
+  --endpoint a3s9vru41x0aa1-ats.iot.us-east-1.amazonaws.com \
+  --cert certs/914634da34-certificate.pem.crt \
+  --key certs/914634da34-private.pem.key \
+  --root-ca certs/AmazonRootCA1.pem \
+  --client-id basicPubSub \
+  --topic test/Python
